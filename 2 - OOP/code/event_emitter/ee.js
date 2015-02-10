917a545bf8a0de515c9bdc0dc02a77386593bece
Roman Melnyk <aywengo@gmail.com>
2015-02-10 22:05:19 +0100

    Integrated with IntellyJ and karma

--- 2 - OOP/code/event_emitter/ee.js
+++ 2 - OOP/code/event_emitter/ee.js
@@ -6,31 +6,65 @@
 	}
 
 	EE = function () {
-		//store the listeners somewhere
 		this.listeners = {};
+
+        return Object.create(EE.prototype, {
+            listeners: {
+                value: this.listeners
+            }
+        });
 	};
 
 	EE.prototype.on = function (eventName, listener, context) {
+        var that = this;
 
+        if (typeof this.listeners === "undefined") {
+            this.listeners = {};
+        }
+
+        if (typeof this.listeners[eventName] === "undefined") {
+            this.listeners[eventName] = [];
+        }
+
+        var listenerData = {lstnr: listener, cntx: context};
+        this.listeners[eventName].push(listenerData);
+
+        return function () {
+            if (that.listeners && typeof that.listeners[eventName] !== "undefined") {
+                for (var i = 0; i < that.listeners[eventName].length; i++) {
+                    if (that.listeners[eventName][i] === listenerData) {
+                        that.listeners[eventName].splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
 	};
 
 	EE.prototype.emit = function (eventName, data) {
+        var args = Array.prototype.splice.call(arguments, data);
 
-	};
+        if (typeof this.listeners[eventName] !== "undefined") {
+            this.listeners[eventName].forEach(function (callback) {
+                callback.lstnr.apply(callback.cntx, args);
+            })
+        }
+    };
 
-//	var ee = new EE();
-//  var customCtx = { key: value }
-//
-//	var removeListener = ee.on('test', function (arg1, arg2) {
-//		console.log(arg1, arg2); //1, 2
-//		console.log(this.key); //value
-//	}, customCtx);
-//
-//	ee.emit('test', [1, 2]);
-//
-//	removeListener(); //removes my listener from the event emitter;
-//
-//	ee.emit('test'); //nothing will execute
+    //not needed
+    //var ee = new EE();
+    //var customCtx = { key: value };
+    //
+    //var removeListener = ee.on('test', function (arg1, arg2) {
+    //	console.log(arg1, arg2); //1, 2
+    //	console.log(this.key); //value
+    //}, customCtx);
+    //
+    //ee.emit('test', [1, 2]);
+    //
+    //removeListener(); //removes my listener from the event emitter;
+    //
+    //ee.emit('test'); //nothing will execute
 
 	global.rule.EventEmitter = EE;
 
--- 2 - OOP/code/new_operator/newOperator.js
+++ 2 - OOP/code/new_operator/newOperator.js
@@ -5,7 +5,24 @@
 
 	//Implement function that will work like a new operator;
 	global.rule.createClassInstance = function (ClassFunction) {
+        if (!global.rule) {
+            global.rule = {};
+        }
 
+        var obj, ret, proto;
+        // Check if `ClassFunction.prototype` is an object, not a primitive
+        proto = Object(ClassFunction.prototype) === ClassFunction.prototype ? ClassFunction.prototype : Object.prototype;
+
+        // Create an object that inherits from `proto`
+        obj = Object.create(proto);
+
+        // Apply the function setting `obj` as the `this` value
+        ret = ClassFunction.apply(obj, Array.prototype.slice.call(arguments, 1));
+
+        if (Object(ret) === ret) { // the result is an object?
+            return ret;
+        }
+        return obj;
 	};
 
 
